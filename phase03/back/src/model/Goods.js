08b27f2e7966624eba3507f997acbc1945274ebf
zhengyuj <47991622+zhengyuj@users.noreply.github.com>
2019-04-30 17:33:46 -0400

    Update Goods.js

--- phase03/back/src/model/Goods.js
+++ phase03/back/src/model/Goods.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const uSchema = new Schema(
     {
-        name: { type: String, default: "" }, //
-        title: { type: String, default: "" }, //
-        price: { type: Number, default: 0 }, //
-        desc: { tyoe: String, default: "" },//描述
-        image: String,//图片
+        name: { type: String, default: "" }, 
+        title: { type: String, default: "" }, 
+        price: { type: Number, default: 0 }, 
+        desc: { tyoe: String, default: "" },
+        image: String,
     },
     {
         versionKey: false,
